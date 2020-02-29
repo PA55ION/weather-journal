9dff474f73f0355562f08847111f65fdf00cac05
Mai Campbell <jirallc@gmail.com>
2020-02-28 21:03:23 -0800

    feat: create update UI function

--- website/app.js
+++ website/app.js
@@ -2,6 +2,8 @@
 const submitBtn = document.getElementById('generate');
 const feelings = document.getElementById('feelings');
 const zipCode = document.getElementById('zip');
+// const weatherContainer = document.getElementsByClassName('weather');
+// const gridContainer = document.getElementsByClassName('grid-container')
 // Personal API Key for OpenWeatherMap API
 const API_KEY = '050fe05f4b6fa9f366de526cee98af35';
 const api = `https://api.openweathermap.org/data/2.5/weather?zip=`;
@@ -33,7 +35,6 @@ getDate();
 submitBtn.addEventListener('click', getWeather);
 
 submitBtn.addEventListener('click', () => {
-    if (zipCode !== 'value') 
     zipCode.placeholder = 'Your zip is empty'
 })
 
@@ -50,12 +51,15 @@ function getWeather(e) {
             zipCode: zipCode,
             feelings: feelings,
             weather_code: data.weather[0].id,
+            description: data.weather[0].description,
             temp: Math.floor(data.main.temp),
             feels_like: Math.floor(data.main.feels_like),
             temp_min: Math.floor(data.main.temp_min),
             temp_max: Math.floor(data.main.temp_max),
             humidity: Math.floor(data.main.humidity),
             wind: Math.round(data.wind.speed),
+            sunrise: data.sys.sunrise,
+            sunset: data.sys.sunset,
             country: data.sys.country,
             city: data.name
         });
@@ -72,7 +76,6 @@ const weather = async( url = '')  => {
         const data = await request.json();
         console.log('Get Response:', data)
         return data;
-        updateUI(data)
     }
     catch(error) {
         console.log('error', error)
@@ -92,6 +95,7 @@ const postWeather = async(url = '', data={}) => {
     try {
         console.log("post response:", response);
         console.log(response);
+        updateUI(data)
     } 
     catch(error) {
         console.log('error', error)
@@ -104,12 +108,48 @@ function updateUI(data) {
         let temp = document.getElementById('temp');
         let city = document.getElementById('city');
         let description = document.getElementById('content');
-        let feels = document.getElementById('feel');
+        let feels = document.getElementById('feels');
         let wind = document.getElementById('wind');
         let humidity = document.getElementById('humidity');
         const minMaxTemp = document.getElementById('min-max-temp');
         const sunrise = document.getElementById('sunrise');
         const sunset = document.getElementById('sunset')
+        let weatherIcon = document.getElementById('weather-icons')
+        let weatherClass = `<i class="wi wi-owm-${data.weather_code}" ></i>`;
+        let weatherContainer = document.getElementsByClassName('weather-container');
+        
 
-        document.getElementById('temp').innerHTML = data.temp
+        //convert unix time stamp for sunrise and sunset
+        
+        let unix_time = data.sunset && data.sunrise
+        let date = new Date(unix_time * 1000)
+        let hours = date.getHours();
+        // Minutes part from the timestamp
+        let minutes = "0" + date.getMinutes();
+        let ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12;
+        let formattedTime = hours + ':' + minutes.substr(-2)  + ampm;
+
+        
+        temp.innerHTML = `<div id="temp"> ${data.temp} <i class="wi wi-fahrenheit"></i></div>`;
+        city.innerHTML = `${data.city}, ${data.country}`
+        description.innerHTML = data.description;
+        wind.innerHTML =  `<i class="wi wi-strong-wind"></i>;
+        <br /><p>Wind<br />${data.wind}</p>`;
+        feels.innerHTML = `<i class="wi wi-thermometer"></i>;
+        <br /><p>Feel Like<br />${data.feels_like}</p>`;
+        weatherIcon.innerHTML = `${weatherClass}`
+        humidity.innerHTML = ` <i class="wi wi-humidity"></i>;
+        <br /><p>Humidity<br />${data.humidity}</p>`
+        minMaxTemp.innerHTML = `<i class="wi wi-barometer"></i>;
+        <br /><p>Min | Max <br /> ${data.temp_min} | ${data.temp_max}</p>`;
+        sunrise.innerHTML = `<i class="wi wi-sunrise"></i>
+        <br /><p>Sunrise<br />${formattedTime}</p>`
+        sunset.innerHTML = ` <i class="wi wi-sunset"></i>
+        <br /><p>Sunset<br /></p>`;
+
+        
+
+      
 }
